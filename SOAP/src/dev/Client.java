9245ff54d10a047c464eb438473c188f0a5df644
andy_seaborne <dev-null@apache.org>
2005-07-02 17:51:00 +0000

    Test client for proto SOAP service
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Joseki-CVS/Joseki3/trunk@1114709 13f79535-47bb-0310-9956-ffa450edef68

---
+++ SOAP/src/dev/Client.java
@@ -0,0 +1,42 @@
+package dev;
+
+import org.apache.axis.client.Call;
+import org.apache.axis.client.Service;
+
+//import javax.xml.namespace.QName;
+
+public class Client
+{
+	public static void main(String [] args)
+	{
+		try {
+			String endpoint = 
+				"http://localhost:8080/axis/services/WSquery" ;
+			
+			Service  service = new Service();
+			Call     call    = (Call) service.createCall();
+			
+			call.setTargetEndpointAddress( new java.net.URL(endpoint) );
+			call.setOperationName(new QName("http://soapinterop.org/", "echoString") );
+			
+			//call.addParameter("testParam",
+			//                  org.apache.axis.Constants.XSD_STRING,
+			//                  javax.xml.rpc.ParameterMode.IN);
+			//call.setReturnType(org.apache.axis.Constants.XSD_STRING);
+
+			String queryString = "SELECT" ;
+			String defaultGraphURI = "<default graph>" ;
+			String[] namegraphURIs = new String[]{"<g1>", "<g2>"} ;
+			Object[] args = 
+				new Object[]{queryString, defaultGraphURI, namegraphURIs} ;
+
+			String ret = (String)call.invoke(args) ;
+
+           System.out.println("Sent , got '" + ret + "'");
+
+       } catch (Exception e) {
+           System.err.println(e.getMessage()) ;
+		   e.printStacktrace(System.err) ;
+       }
+   }
+}
