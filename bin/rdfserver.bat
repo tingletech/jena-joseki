9a445e1251252510dc915917510d63dd1c2da29c
andy_seaborne <dev-null@apache.org>
2006-11-16 11:43:25 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Joseki-CVS/Joseki3/trunk@1114958 13f79535-47bb-0310-9956-ffa450edef68

--- bin/rdfserver.bat
+++ bin/rdfserver.bat
@@ -2,9 +2,8 @@
 @REM $Id: rdfserver.bat,v 1.4 2004/04/13 15:49:23 andy_seaborne Exp $
 
 if not "%JOSEKIROOT%" == "" goto :ok
-
 echo Environment variable JOSEKIROOT not set
-goto theEnd
+goto :theEnd
 
 :ok
 call bin\joseki_path
@@ -13,6 +12,12 @@ REM set LOGCONFIG=file:%JOSEKIROOT%\etc\log4j-detail.properties
 set LOGCONFIG=file:%JOSEKIROOT%\etc\log4j.properties
 set LOG=-Dlog4j.configuration=%LOGCONFIG%
 
-java -cp %CP% %LOG% joseki.rdfserver %1 %2 %3 %4 %5 %6 %7 %8 %9
+set JAVA=java.exe
+if "%JAVA_HOME%" == "" goto :endJavaHome
+set JAVA=%JAVA_HOME%\bin\java.exe
+
+:endJavaHome
+
+%JAVA% -cp %CP% %LOG% joseki.rdfserver %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 :theEnd
