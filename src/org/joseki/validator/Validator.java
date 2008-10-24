e60fc812951c885572d6f17a42e323cc5e4d68eb
andy_seaborne <dev-null@apache.org>
2008-10-24 15:28:52 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jena/Import/Joseki-CVS/Joseki3/trunk@1115103 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/joseki/validator/Validator.java
+++ src/org/joseki/validator/Validator.java
@@ -191,7 +191,7 @@ public class Validator extends HttpServlet
             if ( query != null && outputAlgebra )
             {
                 outStream.println("<p>Algebra structure:</p>") ;
-                final Op op = Algebra.compile(query, false) ;   // No optimization
+                final Op op = Algebra.compile(query) ;   // No optimization
                 final SerializationContext sCxt = new SerializationContext(query) ;
                 Content c = new Content(){
                     public void print(IndentedWriter out)
@@ -203,7 +203,7 @@ public class Validator extends HttpServlet
             if ( query != null && outputQuads )
             {
                 outStream.println("<p>Quad structure:</p>") ;
-                final Op op = Algebra.toQuadForm(Algebra.compile(query, false)) ;
+                final Op op = Algebra.toQuadForm(Algebra.compile(query)) ;
                 final SerializationContext sCxt = new SerializationContext(query) ;
                 Content c = new Content(){
                     public void print(IndentedWriter out)
